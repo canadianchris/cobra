389d2470810d7db783589a0e6f05e9cab9b59202
Chris Saunders <chrisb@redhat.com>
2018-02-13 15:47:24 -0500

    Initial pseudo-code. For those following along at home, I have no idea what I'm doing for the most part. :)

---
+++ main.py
@@ -0,0 +1,40 @@
+"""
+Playing with Python to create a basic container management system. My goal is to allow basic CRUD functions for linux containers
+"""
+
+class net_ns:
+    # stub for network namespace class
+
+class pid_ns:
+    # stub for pid namespace class
+
+class mount_ns:
+    # stub for mount namespace class
+
+class ipc_ns:
+    # stub for ipc namespace class
+
+class user_ns:
+    # stub for user namespace class
+
+class cgroup:
+    # stub for cgroup class
+
+def get_container_parameters():
+    # stub for getting parameters of containers from users
+
+def parse_container_definition():
+    # stub for code to parse YAML based config file for container definition
+
+def build_namespace():
+    # stub for code to build namespaces
+
+def create_cgroups():
+    # stub for code to build cgroups
+
+def main():
+    # woot
+
+if __name__ == '__main__':
+    main()
+    
